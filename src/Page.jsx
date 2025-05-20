31894032646b078ee405a5348c02ddbe3f219f63
suhas <suhas0205@gmail.com>
2025-05-20 13:58:27 +0530

    changed p tag to h3

--- src/Page.jsx
+++ src/Page.jsx
@@ -5,7 +5,7 @@ const Page=({currentPage, totalPages, previous, next})=>{
     return(
         <div style={{display:'flex', justifyContent:"space-around", alignItems:'center', width:300}}>
             <button id="Previous"onClick={previous} disabled={currentPage === 1}>Previous</button>
-            <p>{currentPage}</p>
+            <h3>{currentPage}</h3>
             <button id='Next' disabled={currentPage === totalPages} onClick={next}>Next</button>
 
 
