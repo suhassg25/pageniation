a11348759fefac7600b0ca1dc5a72ece7f6a8388
suhas <suhas0205@gmail.com>
2025-05-20 14:31:53 +0530

    removed disabled

--- src/Page.jsx
+++ src/Page.jsx
@@ -4,9 +4,9 @@ import React from 'react';
 const Page=({currentPage, totalPages, previous, next})=>{
     return(
         <div style={{display:'flex', justifyContent:"space-around", alignItems:'center', width:300}}>
-            <button id="Previous"onClick={previous} disabled={currentPage === 1}>Previous</button>
+            <button id="Previous"onClick={previous}>Previous</button>
             <h3>{currentPage}</h3>
-            <button id='Next' disabled={currentPage === totalPages} onClick={next}>Next</button>
+            <button id='Next' onClick={next}>Next</button>
 
 
         </div>
